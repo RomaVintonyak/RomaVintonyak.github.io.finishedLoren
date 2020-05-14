88953848e368d8acb3b81caa17101a1f8a508654
RomaVintonyak <19roman23@gmail.com>
2020-05-14 12:32:31 +0300

    create working contact form

--- ajax/mail.php
+++ ajax/mail.php
@@ -1,30 +1,14 @@
 <?php
-$name = trim(strip_tags($_POST['name']));
-$mail = trim(strip_tags($_POST['mail']));
-$phone = trim(strip_tags($_POST['phone']));
-$massage = trim(strip_tags($_POST['mesag']));
+$email = trim(strip_tags($_POST['email']));
+$message = '<h3 style="margin: 0; padding: 0; font-size: 18px; font-weight: 700">Дані повідомлення:</h3>'.'<br/>';
+$message .= '<span style="margin: 0; padding: 0; font-size: 14px; font-weight: 700; color: #896052">Ім`я: </span>'.'<strong>'.trim(strip_tags($_POST['name'])).'</strong>'.'<br/>';
+$message .= '<span style="margin: 0; padding: 0; font-size: 14px; font-weight: 700; color: #896052">Телефон: </span>'.'<strong>'.'<a href="tel:'.trim(strip_tags($_POST['phone'])).'">'.trim(strip_tags($_POST['phone'])).'</a>'.'</strong>'.'<br/>';
+$message .= '<span style="margin: 0; padding: 0; font-size: 14px; font-weight: 700; color: #896052">Email адреса: </span>'.'<strong>'.trim(strip_tags($_POST['email'])).'</strong>'.'<br/>';
+$message .= '<span style="margin: 0; padding: 0; font-size: 14px; font-weight: 700; color: #896052">Текст повідомлення: </span>'.'<strong>'.trim(strip_tags($_POST['message'])).'</strong>'.'<br/>';
 
-/*$name_tema = "=?utf-8?b?". base64_encode($name) ."?=";*/
+$subject = "=?utf-8?B?".base64_encode("Повідомлення із сайту декору loren.if.ua")."?=";
+$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n"."X-Mailer: PHP/".phpversion();
 
-/*$subject ="Нове повідомлення із сайту loren.if.ua";
-$subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
-
-$message1 ="\n\nІмя: ".$name."\n\nТелефон: " .$phone."\n\nE-mail: " .$mail."\n\nПовідомлення: ".$massage."\n\n";
-
-$header = "Content-Type: text/html; charset=utf-8\n";
-
-$header .= "From: Нове повідомлення <loren.decor@ukr.net>\n\n";
-$success = mail("loren.decor@ukr.net", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));*/
-
-$subject = "=?utf-8?B?".base64_encode("Повідомлення із сайту декору.")."?=";
-$headers = "From: $mail\r\nReply-to: $mail\r\nContent-type: text/html; charset=utf-8\r\n";
-
-$mass = "gh";
-$text = "\n\nІмя: ".$name."\n\nТелефон: " .$phone."\n\nE-mail: " .$mail."\n\nПовідомлення: ".$mass."\n\n";
-
-
-$success = mail("loren.decor@ukr.net",  $subject, $massage, $headers);
-echo $success;
-
-
+$success = mail("loren.decor@ukr.net", $subject, $message, $headers);
+echo "Дякуємо за Ваше звернення!";
 ?>
